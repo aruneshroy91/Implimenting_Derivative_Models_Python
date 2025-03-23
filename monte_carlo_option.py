607a821ac5366ea395a3e8455d95522d31d1b77d
aruneshroy91 <arunesroy@gmail.com>
2025-03-23 21:26:13 +0530

    barrier

--- monte_carlo_option.py
+++ monte_carlo_option.py
@@ -4,7 +4,7 @@ from scipy.stats import norm
 
 
 class monte_carlo:
-    def __init__(self, S0, K, r, T, sigma, dlta, N, numiter):
+    def __init__(self, S0, K, r, T, sigma, dlta, N, rho, H, numiter):
         self.S0 = S0
         self.K = K
         self.r = r
@@ -13,6 +13,8 @@ class monte_carlo:
         self.N = N
         self.dlta = dlta
         self.numiter = numiter
+        self.rho = rho
+        self.H = H
 
     def asset_price(S0, r, T, sigma, dlta, N):
         S = np.zeros(N)
@@ -55,8 +57,33 @@ class monte_carlo:
                 S[i,0] = S[i-1,0] * np.exp(nudt + sigmadt * z[i])
                 S[i,1] = S[i-1,1] * np.exp(nudt + sigmadt * (-z[i]))
             return S
+    
+    def correlated_price(S0, r, T, sigma, dlta, N, rho):
+            # nu1, nu2 =  nu
+            # sigma1, sigma2 = sigma
+            S = np.zeros([N,2])
+            z1 = np.random.normal(loc=0,scale=1,size=N)
+            z2 = rho * z1 + np.sqrt(1-rho**2) * z1
+            S[0,:] = S0
+            dt = T/N
+            nudt = (r-dlta-0.5*sigma**2)*dt
+            sigmadt = sigma*np.sqrt(dt)
+            for i in range(1,N):
+                S[i,0] = S[i-1,0] * np.exp(nudt + sigmadt * z1[i])
+                S[i,1] = S[i-1,1] * np.exp(nudt + sigmadt * (z2[i]))
+            return S
         
-
+    def mc_barrier_call(S_mc, r, T, N, numiter, H, K):
+        call_mc = np.zeros((N,numiter))
+        C0expected = np.zeros(numiter)
+        for i in range(numiter):
+            for j in range(N):
+                if S_mc[j,i] <= H:
+                    call_mc[j,i] = 0
+                else:
+                    call_mc[j,i] = monte_carlo.call(S_mc[j,i],K)
+            C0expected = np.exp(-r*T)*(1/numiter)*np.sum(call_mc, axis=1)
+        return C0expected
 
 
 
